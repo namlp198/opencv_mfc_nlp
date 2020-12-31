5956033f67a433eb2866a76cbee1fdef25f5422b
Hyung Jun Park <hjparkg71@gmail.com>
2020-12-31 10:43:52 +0900

    Add project files.

---
+++ global_header.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int facedetect_image(void);
---
+++ opencv_mfc.h
@@ -0,0 +1,32 @@
+
+// opencv_mfc.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CopencvmfcApp:
+// See opencv_mfc.cpp for the implementation of this class
+//
+
+class CopencvmfcApp : public CWinApp
+{
+public:
+	CopencvmfcApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CopencvmfcApp theApp;
